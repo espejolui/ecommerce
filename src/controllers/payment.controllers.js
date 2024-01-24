c4a85974e68a7e18394eab83cfe462f67523b24a
luis empresa <hola@luisespejo.com>
2024-01-24 17:24:36 -0500

    Controladores

---
+++ src/controllers/payment.controllers.js
@@ -0,0 +1,9 @@
+export const createOrder = (req, res) => res.send('Creando orden')
+
+export const success = (req, res) => {
+    (req, res) => res.send('Success')
+}
+
+export const webhook = (req, res) => {
+    (req, res) => res.send('Webhook')
+}
